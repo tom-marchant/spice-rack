78cf3d27e352732edcd231b8fe97db02632aadcc
tom-marchant <tom.marchant@gmail.com>
2019-01-23 22:15:49 +0000

    First commit

---
+++ test/widget_test.dart
@@ -0,0 +1,28 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:spice_rack/main.dart';
+
+void main() {
+  testWidgets('Loads the app', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(SpiceRackApp());
+
+    expect(find.text('Spice Rack'), findsOneWidget);
+    expect(find.byType(ListView), findsOneWidget);
+    expect(find.byIcon(Icons.add), findsOneWidget);
+  });
+
+  testWidgets('Shows full list of available consumables', (WidgetTester tester) async {
+    await tester.pumpWidget(Consumables());
+
+    expect(find.byType(ListView), findsOneWidget);
+  });
+}
